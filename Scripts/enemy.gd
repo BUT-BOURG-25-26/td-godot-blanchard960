32ff04d26edd40984e119249d8c2d72e9f036357
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-10-07 17:26:23 +0200

    déplacements et look_at

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,21 @@
 extends CharacterBody3D
 
-@export var move_speed:float = 1
+@export var move_speed:float = 3
 var player 
+var look_at_position
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+	look_at_position = player.global_position
 
 func _physics_process(delta: float) -> void:
 	var move = Vector3(player.global_position.x-global_position.x,0.0,player.global_position.z-global_position.z).normalized()
 	velocity = move * move_speed
+	
+	velocity.y = get_gravity().y
+	
+	
+	look_at_position.y = position.y
+	look_at(look_at_position)
+	
 	move_and_slide()
