ceaf1fc2f5d4fd41822715cd6b9907b4b1d9deba
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-10-14 11:58:39 +0200

    rectification spawn enemy

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -2,20 +2,19 @@ extends Node3D
 
 #permet de référencer votre scène enemy au sein de votre script
 @export var enemy_scene: PackedScene
-@export var distance_from_player : int = 10
+@export var min_spawn_distance : int =5
+@export var max_spawn_distance : int =10
 var player : Player
 var rng = RandomNumberGenerator.new()
 
 func _on_timer_timeout():
 	player = get_tree().get_first_node_in_group("player")
-	var x = rng.randi_range(-distance_from_player, distance_from_player)
-	var z = rng.randi_range(-distance_from_player, distance_from_player)
-	if( -5 > (player.global_position.x-x)+(player.global_position.z-z) || (player.global_position.x-x)+(player.global_position.z-z) > 5) :
-		# Spawner votre enemy
-		var enemy = enemy_scene.instantiate()
-		get_parent().add_child(enemy)
-		enemy.position = player.global_position +Vector3(x,0.0,z)
-	else:
-		var enemy = enemy_scene.instantiate()
-		get_parent().add_child(enemy)
-		enemy.position = player.global_position +Vector3(x+5,0.0,z-5)
+	
+	var distance : int = rng.randi_range(min_spawn_distance, max_spawn_distance)
+	
+	var x = 1 +rng.randi_range(-1, 1)
+	var z = 1 + rng.randi_range(-1, 1)
+		
+	var enemy = enemy_scene.instantiate()
+	get_parent().add_child(enemy)
+	enemy.position = player.global_position + (distance * Vector3(x,0.0,z).normalized())
